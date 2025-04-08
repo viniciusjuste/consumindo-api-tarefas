ab1a30b5e525cd8e784eb80f4e4b9407b72aa9fb
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-04-08 19:36:28 -0300

    Fiz o método POST, já consigo criar uma terefa

--- consumindoApiTarefas/src/app/app.component.ts
+++ consumindoApiTarefas/src/app/app.component.ts
@@ -1,16 +1,43 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { Task } from '../../models/Task';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'consumindoApiTarefas';
+  url = 'http://localhost:5257/tasks';
 
-  constructor(private http : HttpClient) {}
+  task$!: Observable<Task>;
+
+  inputName = '';
+  inputIsCompleted : boolean | null = null;
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Adds a new task using the input values for id, name, and completion status.
+   * Sends a POST request to the server with the new task details.
+   * After a successful response, logs the task to the console and resets the input fields.
+   */
+  addTask() {
+    const newTask = {
+      name: this.inputName,
+      isCompleted: this.inputIsCompleted
+    };
+
+    this.http.post<Task>(`${this.url}/add`, newTask).subscribe((task) => {
+      console.log(`Task added: ${task.name}`);
+      this.inputName = '';
+      this.inputIsCompleted = false;
+    });
+  }
 
 }
