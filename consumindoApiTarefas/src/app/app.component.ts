f4ef85b4e98b5400565cef5408a3abf0705af59e
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-04-08 18:37:49 -0300

    Declarando o Modulo HTTP

--- consumindoApiTarefas/src/app/app.component.ts
+++ consumindoApiTarefas/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,4 +10,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'consumindoApiTarefas';
+
+  constructor(private http : HttpClient) {}
+
 }
